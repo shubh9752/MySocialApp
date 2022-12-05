269cdc4e30ac059ae8cb45c6bc0b5504e0051ab3
shubh9752 <61161071+shubh9752@users.noreply.github.com>
2022-12-05 09:49:34 +0530

    controllers use async or await

--- controllers/Home_contr.js
+++ controllers/Home_contr.js
@@ -1,27 +1,28 @@
 const Post = require("../models/post");
 //const { post } = require("../routers");
-const User=require("../models/user")
+const User = require("../models/user");
 
-module.exports.home = (req, res) => {
+module.exports.home = async (req, res) => {
   // console.log(req.cookies)
-  Post.find({})
-    .populate("user")
-    .populate({
-      path:"comment",
-      populate:{
-        path:"user"
-      }
-    })
-    .exec((err, post) => {
-      User.find({},(err,users)=>{
-        return res.render("home", {
-          title: "Socially",
-          description: "this is home page of socaially",
-          post: post,
-          allUsers:users
-        });
+  try {
+    let post = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comment",
+        populate: {
+          path: "user",
+        },
+      });
 
-      })
-     
+    let users = await User.find({});
+    return res.render("home", {
+      title: "Socially",
+      description: "this is home page of socaially",
+      post: post,
+      allUsers: users,
     });
+  } catch (err) {
+    console.log("this error creating problem: ");
+    return;
+  }
 };
--- controllers/comment_contr.js
+++ controllers/comment_contr.js
@@ -3,52 +3,61 @@ const Post=require("../models/post");
 const mongoose=require("mongoose");
 const { ObjectId } = require('mongodb');
 
-module.exports.create=(req,res)=>{
-    console.log(req.body.post);
+module.exports.create=async (req,res)=>{
+    try{
+         //console.log(req.body.post);
     let id=ObjectId((req.body.post).trim());
-    Post.findById(id,(err,post)=>{
+    let post=await Post.findById(id);
         // if(err){
         //     console.log(err)
         // }
         if(post){
            // console.log(" erro in line 12"+req.body.content+" "+req.user._id)
-            Comment.create({
+            let comment=await Comment.create({
                 content:req.body.content,
                 post:req.body.post,
                 user:req.user._id
 
-            },(err,comment)=>{
-                if(err){
-                    console.log("there is an error in post comment",err);
-                    return;
-                };
+            });
                 post.comment.push(comment);
                 post.save();
 
                 res.redirect("/");
-            });
-           
-        }else{
-            console.log("inside else")
-        };
-    });
+            };
 
+    }catch(err){
+        console.log(`err in creating post: ${err}`);
+        return;
+
+    }
+   
+           
+       
+   
 };
 
-module.exports.delete=(req,res)=>{
-    Comment.findById(req.params.id,(err,comment)=>{
-        console.log("line 40")
-        console.log(comment.user)
+module.exports.delete=async(req,res)=>{
+    try{
+        let comment=await Comment.findById(req.params.id);//=>{
+    //     console.log("line 40")
+    //     console.log(comment.user)
         if(comment.user == req.user.id){
         ///    console.log(user +" this is user error")
             let postId=comment.post;
             comment.remove();
-            Post.findByIdAndUpdate(postId,{$pull:{comment:req.params.id}},(err,post)=>{
-                return res.redirect("back");
-            });
+             let post=await Post.findByIdAndUpdate(postId,{$pull:{comment:req.params.id}});//,(err,post)=>{
+              return res.redirect("back");
+            // });
         }else{
             return res.redirect("back")
         }
 
-    });
+    }catch(err){
+        console.log(`err in deleting comment ${err}`);
+        return;
+
+    }
+    
+     
+
 };
